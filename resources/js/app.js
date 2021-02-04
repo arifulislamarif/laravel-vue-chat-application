230318c3fa759274d41305147e90b769999fd137
arifulislamarif <arif14arif15@gmail.com>
2021-02-04 14:03:51 +0600

    vuex integrated

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,14 @@
 require('./bootstrap');
-
 window.Vue = require('vue');
 
 Vue.component('main-app', require('./components/MainApp.vue').default);
 
+import StoreApp from './store/index'
+import Vuex from 'vuex'
+const store = new Vuex.Store(StoreApp);
+Vue.use(Vuex)
+
 const app = new Vue({
-    el: '#app',
+    el: "#app",
+    store
 });
