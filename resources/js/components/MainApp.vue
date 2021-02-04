a7cf739752ad2d38c399f1440f34220b2b62fd89
arifulislamarif <arif14arif15@gmail.com>
2021-02-04 13:34:11 +0600

    message page content loaded with vue app

---
+++ resources/js/components/MainApp.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="main-app">
+        <ChatComponent/>
+    </div>
+</template>
+
+<script>
+import ChatComponent from './ChatApp'
+export default {
+    components: {
+        ChatComponent,
+    }
+}
+</script>
